e038025907cc029f00bc302d3a18adf9f4e06bbf
Ethan Bellmer <e.bellmer@outlook.com>
2020-04-18 19:47:23 +0100

    Implement fauxmoESP library for Alexa switching

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,47 @@
 #include "credentials.h"
 
 
-// Declare pin position for LED, relay, and push button
-int ledPin = 2;
-//int relayPin = 5;
-int buttonPin = 16;
-
 // Instatiate with a false power and button sate.
 // This is used for tracking power on/off state between button and Alexa uses. 
 boolean powerState = false;
 int lastButtonState = 0;
 
-
+// Declare pin position for LED, relay, and push button
 #define SERIAL_BAUDRATE 115200
-#define relayPin 5
+#define relayPin 5 // GPIO 5 = NodeMCU D1
 #define relayID "office light"
+#define ledPin 2 // GPIO 2 = NodeMCU D4
+#define ledID "button LED"
+#define buttonPin 16 // GPIO 16 = NodeMCU D0
+#define buttonID "button pin"
 
-
+// Instantiate new fauxmoESP object
 fauxmoESP fauxmo;
 
+
+/***
+ * Turn on callback that turns on the relay for the lights
+ * and turns off the indicator LED.
+ */
+void lightsOn() 
+{
+	digitalWrite(relayPin, HIGH); // turn on relay
+	digitalWrite(ledPin, LOW); // turn off LED
+	Serial.println("Turning on lights");
+	powerState = true; // set the power state to on for the toggle
+}
+
+/***
+ * Turn off callback that turns off the relay for the lights
+ * and turns on the indicator LED.
+ */
+void lightsOff() 
+{
+	digitalWrite(relayPin, LOW); // turn off relay
+	digitalWrite(ledPin, HIGH); // turn on LED
+	Serial.println("Turning off lights");
+	powerState = false; // set the power state to off for the toggle
+}
 
 void wifiSetup() {
 
@@ -47,18 +70,20 @@ void wifiSetup() {
 }
 
 void setup() {
-
 	Serial.begin(SERIAL_BAUDRATE);
 
 	pinMode(ledPin, OUTPUT);
 	pinMode(relayPin, OUTPUT);
 	pinMode(buttonPin, INPUT);
 
+	// Write ledPin high at boot so the LED is on when the lights are off.
 	digitalWrite(ledPin, HIGH);
+	// Write relay pin low at boot so lights remain off when module boots.
 	digitalWrite(relayPin, LOW);
 
-	// Wifi
+	// Run the WiFi setup function and connect to the specified WiFi network. 
     wifiSetup();
+
 
     // By default, fauxmoESP creates it's own webserver on the defined port
     // The TCP port must be 80 for gen3 devices (default is 1901)
@@ -93,6 +118,7 @@ void setup() {
         // Otherwise comparing the device_name is safer.
 
         digitalWrite(relayPin, state ? HIGH : LOW);
+		digitalWrite(ledPin, state ? LOW : HIGH);
 
     });
 
@@ -100,5 +126,39 @@ void setup() {
 }
 
 void loop() {
+	// fauxmoESP uses an async TCP server but a sync UDP server
+    // Therefore, we have to manually poll for UDP packets
 	fauxmo.handle();
+
+
+	// This is a sample code to output free heap every 5 seconds
+    // This is a cheap way to detect memory leaks
+    static unsigned long last = millis();
+    if (millis() - last > 5000) {
+        last = millis();
+        Serial.printf("[MAIN] Free heap: %d bytes\n", ESP.getFreeHeap());
+    }
+
+    // If your device state is changed by any other means (MQTT, physical button,...)
+    // you can instruct the library to report the new state to Alexa on next request:
+    // fauxmo.setState(ID_YELLOW, true, 255);
+
+	// check if button is a new press or a continuation from last loop.
+	if ((digitalRead(buttonPin) == LOW) && (lastButtonState == 1)) 
+	{
+		lastButtonState = 0;
+		if(!powerState)
+		{
+			lightsOn(); // call lightsOn() if lights are off
+		}
+		else
+		{
+			lightsOff(); // call lightsOff() if lights are on
+		}
+	}
+	else if ((digitalRead(buttonPin) == HIGH) && (lastButtonState == 0)) 
+	{
+		lastButtonState = 1; // button released
+	}
+  delay(100); // button bounce delay
 }
